f2449fcfed8ea31ef1ad33da20a8a355a8801539
HurasAdam <huradam@wp.pl>
2026-04-17 23:53:44 +0200

    feat: add password hashing middleware and user model methods

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,5 +1,8 @@
 import { ObjectId } from "mongodb";
 import mongoose, { Schema } from "mongoose";
+import { compareValue, hashValue } from "../../utils/bcrypt";
+
+export type UserWithoutPassword = Omit<UserDocument, "password">;
 export interface UserDocument extends mongoose.Document {
   _id: ObjectId;
   name: string;
@@ -15,6 +18,9 @@ export interface UserDocument extends mongoose.Document {
   lastLogin: Date | null;
   favourites: mongoose.Types.ObjectId[];
   role: string;
+
+  comparePassword(val: string): boolean;
+  omitPassword(): UserWithoutPassword;
 }
 
 const userSchema = {
@@ -29,6 +35,23 @@ const userSchema = {
   isActive: { type: Boolean, default: true },
 };
 const schema = new Schema<UserDocument>(userSchema);
+
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = hashValue(this.password);
+  next();
+});
+
+schema.methods.comparePassword = function (value: string) {
+  return compareValue(value, this.password);
+};
+
+schema.methods.omitPassword = function () {
+  const { password, ...user } = this.toObject();
+  return user;
+};
 
 const UserModel = mongoose.model<UserDocument>("User", schema);
 export default UserModel;
